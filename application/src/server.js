1d1f8e14e5649beeebae480159fcc9d3d31c2a03
Markus <Markus>
2023-12-30 15:42:06 +0200

    refactor server.js

--- application/src/server.js
+++ application/src/server.js
@@ -7,37 +7,37 @@ const router = require('./routes')
 // const bodyParser = require('body-parser');
 
 async function makeServer(cfg) {
+  let clamscan;
   try {
     const newAvConfig = Object.assign({}, cfg.avConfig);
-    const clamscan = await new NodeClam().init(newAvConfig);
-
-    const app = express();
-
-    app.use(cors());
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({ extended: true }));
-    app.use((req, res, next) => {
-      req._av = clamscan;
-      next();
-    });
-
-    app.use(fileUpload({ ...cfg.fuConfig }));
-
-    if (process.env.NODE_ENV !== 'test') {
-        app.use(morgan(process.env.APP_MORGAN_LOG_FORMAT || 'combined'))
-    }
-
-    app.use(router)
-
-    app.all('*', (req, res, next) => {
-      res.status(405).json({ success: false, data: { error: 'Not allowed.' } });
-    });
-
-    return app
-
+    clamscan = await new NodeClam().init(newAvConfig);
   } catch (error) {
     throw new Error(`Cannot initialize clamav object: ${error}`);
   }
+
+  const app = express();
+
+  app.use(cors());
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+    req._av = clamscan;
+    next();
+  });
+
+  app.use(fileUpload({ ...cfg.fuConfig }));
+
+  if (process.env.NODE_ENV !== 'test') {
+      app.use(morgan(process.env.APP_MORGAN_LOG_FORMAT || 'combined'))
+  }
+
+  app.use(router)
+
+  app.all('*', (req, res, next) => {
+    res.status(405).json({ success: false, data: { error: 'Not allowed.' } });
+  });
+
+  return app
 }
 
 module.exports = { makeServer };
